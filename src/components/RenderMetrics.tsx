61921ed87771c233dda6b68b48a1a2b2d4580300
Iván Bautista <ivanbautistag@gmail.com>
2026-05-26 22:44:40 -0600

    feat: add advanced metrics

---
+++ src/components/RenderMetrics.tsx
@@ -0,0 +1,28 @@
+import { useFrame, useThree } from '@react-three/fiber'
+import { useRef } from 'react'
+
+export function RenderMetrics({ onUpdate }) {
+  const { gl, scene } = useThree()
+  const lastUpdate = useRef(0)
+
+  useFrame(({ clock }) => {
+    if (clock.elapsedTime - lastUpdate.current < 10) return // actualiza cada 500ms
+    lastUpdate.current = clock.elapsedTime
+
+    let vertices = 0
+    scene.traverse((obj) => {
+      if (obj.isMesh) vertices += obj.geometry.attributes.position.count
+    })
+
+    onUpdate({
+      drawCalls: gl.info.render.calls,
+      triangles: gl.info.render.triangles,
+      geometries: gl.info.memory.geometries,
+      textures: gl.info.memory.textures,
+      shaders: gl.info.programs?.length ?? 0,
+      vertices,
+    })
+  })
+
+  return null
+}
