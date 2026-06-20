f035b8d3211912725c8c206852db60b762c67ab2
Iván Bautista <ivanbautistag@gmail.com>
2026-06-20 00:52:32 -0600

    refactor: remove postIndex prop and coneColliders, add collisionGroups to stick mesh for ring sensor interaction

--- src/components/colliders/CompundArrowCollider.tsx
+++ src/components/colliders/CompundArrowCollider.tsx
@@ -1,21 +1,33 @@
-import { ConeCollider, CuboidCollider, RigidBody } from '@react-three/rapier'
+import { ConeCollider, CuboidCollider, interactionGroups, RigidBody } from '@react-three/rapier'
 import { ARROW_DATA } from '../../store/model-data'
 
 interface ArrowColliderProps {
   position: [number, number, number]
+  postIndex: number
 }
 
-function ArrowCollider({ position }: ArrowColliderProps) {
+function ArrowCollider({ position, postIndex }: ArrowColliderProps) {
   return (
-    <RigidBody type="fixed" colliders={false} position={position}>
+    <RigidBody type="fixed" colliders={false} position={position} userData={{ postIndex }}>
       {Object.entries(ARROW_DATA).map(([name, c]) => {
-        if (name === 'punta_2' || name === 'tail_1') {
+        if (name === 'tail_1') {
           return (
             <ConeCollider
               key={name}
               args={[c.scale[0], c.scale[1]]}
               position={[c.position[0], c.position[1], c.position[2]]}
               rotation={[c.rotation[0], c.rotation[1], c.rotation[2]]}
+            />
+          )
+        }
+        if (name === 'stick') {
+          return (
+            <CuboidCollider
+              key={name}
+              args={[c.scale[0], c.scale[1], c.scale[2]]}
+              position={[c.position[0], c.position[1], c.position[2]]}
+              rotation={[c.rotation[0], c.rotation[1], c.rotation[2]]}
+              collisionGroups={interactionGroups(0, [1])}
             />
           )
         }
@@ -35,9 +47,9 @@ function ArrowCollider({ position }: ArrowColliderProps) {
 export default function CompundArrowCollider() {
   return (
     <>
-      <ArrowCollider position={[0, -0.115, 0.325]} />
-      <ArrowCollider position={[0, 0, 0]} />
-      <ArrowCollider position={[0, -0.115, -0.313]} />
+      <ArrowCollider position={[0, -0.115, 0.325]} postIndex={0} />
+      <ArrowCollider position={[0, 0, 0]} postIndex={1} />
+      <ArrowCollider position={[0, -0.115, -0.313]} postIndex={2} />
     </>
   )
 }
