225c4867b17d716fea81b05db880119231e56602
Iván Bautista <ivanbautistag@gmail.com>
2026-06-20 00:44:57 -0600

    feat: implement custom hook to apply water jet impulse to rings when positioned within designated zone

---
+++ src/components/hooks/useWaterForce.ts
@@ -0,0 +1,63 @@
+import type { RapierRigidBody } from '@react-three/rapier'
+import type React from 'react'
+import { useGameStore } from '../../store/gameStore'
+import { useFrame } from '@react-three/fiber'
+import { useEffect, useRef } from 'react'
+
+//position: [-0.474, 1.03, -0],
+
+//args: [0.62, 0.075, 0.32],
+export const WATER_ZONE = {
+  centerX: -0.47,
+  centerZ: 0,
+  radius: 1, //horizontal radius
+  minY: 1, //min height
+  maxY: 2, //max height
+  maxStrength: 0.00028,
+  decayRate: 1 / 2, // reach to 0 in 1.2s
+}
+
+export function useWaterForce(rigidBodyRefs: React.RefObject<RapierRigidBody[]>) {
+  const waterActive = useGameStore((state) => state.waterActive)
+  const waterPressureRef = useRef(1)
+  const impulseApplied = useRef(false)
+  const setWaterActive = useGameStore((state) => state.setWaterActive)
+
+  // Dentro de useWaterForce
+  useEffect(() => {
+    if (waterActive) {
+      waterPressureRef.current = 1 // resetea presión al activarse
+      impulseApplied.current = false
+    }
+  }, [waterActive])
+
+  useFrame((_, delta) => {
+    if (!waterActive) return
+    // decay
+    const newPressure = Math.max(0, waterPressureRef.current - WATER_ZONE.decayRate * delta)
+    waterPressureRef.current = newPressure
+
+    if (newPressure === 0) {
+      setWaterActive(false)
+      return
+    }
+
+    rigidBodyRefs.current.forEach((rigidBody) => {
+      if (!rigidBody) return
+      const pos = rigidBody.translation()
+      const dx = pos.x - WATER_ZONE.centerX
+      const dz = pos.z - WATER_ZONE.centerZ
+      const horizontalDist = Math.sqrt(dx * dx + dz * dz)
+      const inZone =
+        horizontalDist < WATER_ZONE.radius && pos.y > WATER_ZONE.minY && pos.y < WATER_ZONE.maxY
+      if (!inZone) return
+
+      const distanceFalloff = Math.max(0, 1 - horizontalDist / WATER_ZONE.radius)
+      const force = (WATER_ZONE.maxStrength / distanceFalloff) * newPressure
+      const randomX = (Math.random() * 2 - 1) * force * 0.4
+      const randomZ = (Math.random() * 2 - 1) * force * 0.4
+
+      rigidBody.applyImpulse({ x: randomX, y: force, z: randomZ }, true)
+    })
+  })
+}
