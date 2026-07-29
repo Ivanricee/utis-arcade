a6deabbda198116b4b31a44863e081c55e4aeb44
Iván Bautista <ivanbautistag@gmail.com>
2026-07-28 21:25:16 -0600

    refactor: update camera position and enhance environment settings in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Environment, OrbitControls, Stage, Stats, useGLTF } from '@react-three/drei'
+import { Environment, OrbitControls, Stats, useGLTF } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import DprManager from './components/DprManager'
 import { Suspense, useState } from 'react'
 import { RenderMetrics } from './components/RenderMetrics'
 import CompoundCollider from './components/colliders/CompounCollider'
 import { ConsoleGame } from './components/ConsoleGame'
-
+import * as THREE from 'three'
 /*function Model() {
   const gltf = useGLTF('/modelos/Untitled.glb')
   return <primitive object={gltf.scene} scale={1} position={[0, 0, 0]} />
@@ -57,31 +57,66 @@ function App() {
             depth: true,
             powerPreference: 'high-performance',
           }}
-          camera={{ position: [5, 3, 5], fov: 45 }}
+          camera={{ position: [0, 0, 5], fov: 45 }}
         >
+          {/**
+            new THREE.Euler(
+              THREE.MathUtils.degToRad(278),
+              THREE.MathUtils.degToRad(67),
+              THREE.MathUtils.degToRad(-15),
+              'YZX'
+            )
+             */}
           <RenderMetrics onUpdate={setMetrics} />
           <Stats showPanel={0} />
           <Stats showPanel={2} className="stats-memory" />
           <DprManager />
-          <Environment preset="warehouse" resolution={256} environmentIntensity={0.6} />
+          <Environment
+            files="/hdri/lighthdri.hdr"
+            //resolution={256}
+            environmentIntensity={0.5}
+            background
+            environmentRotation={
+              new THREE.Euler(
+                THREE.MathUtils.degToRad(0),
+                THREE.MathUtils.degToRad(195),
+                THREE.MathUtils.degToRad(-5),
+                'YZX'
+              )
+            }
+            backgroundRotation={
+              new THREE.Euler(
+                THREE.MathUtils.degToRad(0),
+                THREE.MathUtils.degToRad(195),
+                THREE.MathUtils.degToRad(-5),
+                'YZX'
+              )
+            }
+          />
           <ambientLight intensity={0.3} />
           <Suspense fallback={null}>
             <CompoundCollider />
-            <Stage
-              adjustCamera={false}
-              intensity={0.5}
-              shadows="contact"
-              environment={null}
-            ></Stage>
+            {/**
+               *
+              <Stage
+                adjustCamera={false}
+                intensity={0.5}
+                shadows="contact"
+                environment={null}
+              ></Stage>
+               */}
           </Suspense>
           <Suspense fallback={null}>
+            //
             <ConsoleGame />
-            <Stage
-              adjustCamera={false}
-              intensity={0.5}
-              shadows="contact"
-              environment={null}
-            ></Stage>
+            {/*
+              <Stage
+                adjustCamera={false}
+                intensity={0.5}
+                shadows="contact"
+                environment={null}
+              ></Stage>
+                */}
           </Suspense>
           <OrbitControls enableDamping />
         </Canvas>
