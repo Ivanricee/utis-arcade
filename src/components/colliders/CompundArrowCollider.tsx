607399ccea9ba55448d2a3bdef38fe707dffac56
Iván Bautista <ivanbautistag@gmail.com>
2026-08-01 01:11:47 -0600

    feat: add post mesh with 3 instances synced to RigidBody colliders

--- src/components/colliders/CompundArrowCollider.tsx
+++ src/components/colliders/CompundArrowCollider.tsx
@@ -1,5 +1,8 @@
 import { ConeCollider, CuboidCollider, interactionGroups, RigidBody } from '@react-three/rapier'
 import { ARROW_DATA } from '../../store/model-data'
+import { Instance, Instances, useGLTF, useTexture } from '@react-three/drei'
+import { useEffect, useMemo } from 'react'
+import * as THREE from 'three'
 
 interface ArrowColliderProps {
   position: [number, number, number]
@@ -41,16 +44,46 @@ function ArrowCollider({ position, postIndex }: ArrowColliderProps) {
           />
         )
       })}
+      <Instance />
     </RigidBody>
   )
 }
 
 export default function CompundArrowCollider() {
+  const { nodes: plasticNodes } = useGLTF('/modelos/plastics.glb')
+  const postMMesh = plasticNodes['post'] as THREE.Mesh
+  const lightMap = useTexture('/modelos/textures/plastic/plastic_lightmap.png')
+  useEffect(() => {
+    lightMap.colorSpace = THREE.LinearSRGBColorSpace
+    lightMap.flipY = false
+    lightMap.channel = 1
+    lightMap.needsUpdate = true
+    return () => {
+      lightMap.dispose()
+    }
+  }, [lightMap])
+  const bakedGeometry = useMemo(() => {
+    postMMesh.updateMatrix()
+    const geo = postMMesh.geometry.clone()
+    geo.applyMatrix4(postMMesh.matrix)
+    return geo
+  }, [postMMesh])
+  const postMaterial = useMemo(() => {
+    const material = (
+      Array.isArray(postMMesh.material) ? postMMesh.material[0] : postMMesh.material
+    ).clone() as THREE.MeshStandardMaterial
+    material.lightMap = lightMap
+    material.lightMapIntensity = 1.2
+    material.normalScale?.set(1.7, 1.7)
+    material.emissiveIntensity = 0.9
+    material.needsUpdate = true
+    return material
+  }, [postMMesh])
   return (
-    <>
+    <Instances geometry={bakedGeometry} material={postMaterial} limit={3}>
       <ArrowCollider position={[0, -0.115, 0.325]} postIndex={0} />
       <ArrowCollider position={[0, 0, 0]} postIndex={1} />
       <ArrowCollider position={[0, -0.115, -0.313]} postIndex={2} />
-    </>
+    </Instances>
   )
 }
