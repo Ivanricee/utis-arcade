a7873f6f9108b3944fbc458ed58f8be922fffaff
Iván Bautista <ivanbautistag@gmail.com>
2026-08-13 00:56:58 -0600

    feat: integrate usePlasticMeshes hook in CompoundArrowCollider and CompoundFloatingCollider for improved mesh handling

--- src/components/colliders/CompundArrowCollider.tsx
+++ src/components/colliders/CompundArrowCollider.tsx
@@ -1,8 +1,8 @@
 import { ConeCollider, CuboidCollider, interactionGroups, RigidBody } from '@react-three/rapier'
 import { ARROW_DATA } from '../../store/model-data'
-import { Instance, Instances, useGLTF, useTexture } from '@react-three/drei'
-import { useEffect, useMemo } from 'react'
-import * as THREE from 'three'
+import { Instance, Instances } from '@react-three/drei'
+
+import usePlasticMeshes from '../../hooks/usePlasticMeshes'
 
 interface ArrowColliderProps {
   position: [number, number, number]
@@ -50,40 +50,15 @@ function ArrowCollider({ position, postIndex }: ArrowColliderProps) {
 }
 
 export default function CompundArrowCollider() {
-  const { nodes: plasticNodes } = useGLTF('/modelos/plastics.glb')
-  const postMMesh = plasticNodes['post'] as THREE.Mesh
-  const lightMap = useTexture('/modelos/textures/plastic/plastic_lightmap.png')
-  useEffect(() => {
-    lightMap.colorSpace = THREE.LinearSRGBColorSpace
-    lightMap.flipY = false
-    lightMap.channel = 1
-    lightMap.needsUpdate = true
-    return () => {
-      lightMap.dispose()
-    }
-  }, [lightMap])
-  const bakedGeometry = useMemo(() => {
-    postMMesh.updateMatrix()
-    const geo = postMMesh.geometry.clone()
-    geo.applyMatrix4(postMMesh.matrix)
-    return geo
-  }, [postMMesh])
-  const postMaterial = useMemo(() => {
-    const material = (
-      Array.isArray(postMMesh.material) ? postMMesh.material[0] : postMMesh.material
-    ).clone() as THREE.MeshStandardMaterial
-    material.lightMap = lightMap
-    material.lightMapIntensity = 1.2
-    material.normalScale?.set(1.7, 1.7)
-    material.emissiveIntensity = 0.9
-    material.needsUpdate = true
-    return material
-  }, [postMMesh])
+  const { geometries, material } = usePlasticMeshes()
+
   return (
-    <Instances geometry={bakedGeometry} material={postMaterial} limit={3}>
-      <ArrowCollider position={[0, -0.115, 0.325]} postIndex={0} />
-      <ArrowCollider position={[0, 0, 0]} postIndex={1} />
-      <ArrowCollider position={[0, -0.115, -0.313]} postIndex={2} />
-    </Instances>
+    <>
+      <Instances geometry={geometries.post} material={material} limit={3}>
+        <ArrowCollider position={[0, -0.115, 0.325]} postIndex={0} />
+        <ArrowCollider position={[0, 0, 0]} postIndex={1} />
+        <ArrowCollider position={[0, -0.115, -0.313]} postIndex={2} />
+      </Instances>
+    </>
   )
 }
--- src/components/colliders/CompundFloatingCollider.tsx
+++ src/components/colliders/CompundFloatingCollider.tsx
@@ -1,7 +1,14 @@
 import { CuboidCollider, RigidBody } from '@react-three/rapier'
 import { FLOATING_DATA } from '../../store/model-data'
-
+import usePlasticMeshes from '../../hooks/usePlasticMeshes'
+import { Instance, Instances } from '@react-three/drei'
+const positions = [
+  [0, 0.8, 0.3],
+  [0, 0.7, 0.1],
+  [0, 0.9, 0.4],
+] as [number, number, number][]
 export function CompoundFloatingCollider() {
+  const { geometries, material } = usePlasticMeshes()
   return (
     <>
       {(Object.keys(FLOATING_DATA) as Array<keyof typeof FLOATING_DATA>).map((key) => {
@@ -13,6 +20,16 @@ export function CompoundFloatingCollider() {
               position={[data.position[0], data.position[1], data.position[2]]}
               rotation={[data.rotation[0], data.rotation[1], data.rotation[2]]}
             />
+            <mesh geometry={geometries.balloon} material={material} position={[0, 0.25, 0]} />
+            <mesh geometry={geometries.balloon} material={material} position={[0, 0.2, -1.5]} />
+            <Instances geometry={geometries.octo} material={material} limit={20}>
+              {positions.map((pos) => (
+                <Instance scale={[0.45, 0.45, 0.45]} position={pos} key={pos.join(',')} />
+              ))}
+
+              {/* etc */}
+            </Instances>
+            <mesh geometry={geometries.zeppe} material={material} />
           </RigidBody>
         )
       })}
