232c93fca467d289d29bd34f489b96f6447ce305
Iván Bautista <ivanbautistag@gmail.com>
2026-08-20 00:05:38 -0600

    refactor: remove unused imports and comments in ConsoleGame component

--- src/components/ConsoleGame.tsx
+++ src/components/ConsoleGame.tsx
@@ -3,10 +3,7 @@ import { useGLTF, useTexture } from '@react-three/drei'
 import { useGameStore } from '../store/gameStore'
 import { useEffect } from 'react'
 import * as THREE from 'three'
-//import { WATER_ZONE } from '../hooks/useWaterForce'
-/**
- 1 Mari aumentar los nromales
- */
+
 export function ConsoleGame() {
   const { scene } = useGLTF('/modelos/base.glb')
   const lightMap = useTexture('/modelos/textures/base/bakedLightBase.png')
