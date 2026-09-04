b84d8555010ab7ff770f8c341c08e7e7fb9a87d1
Iván Bautista <ivanbautistag@gmail.com>
2026-09-04 00:31:30 -0600

    refactor: adjust floating elements spawn position

--- src/store/spawnBounds.ts
+++ src/store/spawnBounds.ts
@@ -3,11 +3,17 @@ export interface SpawnBounds {
   y: [number, number]
   z: [number, number]
 }
-
+//spawns
 export const LEFT_ZONE_BOUNDS: SpawnBounds = {
-  x: [-0.58, -0.35],
-  y: [1.501, 2.25],
+  //x: [-0.52, -0.45],
+  x: [-0.52, -0.45],
+  y: [1.38, 1.39],
   z: [0.75, 0.8604],
+}
+export const RIGHT_ZONE_BOUNDS: SpawnBounds = {
+  x: [-0.52, -0.45],
+  y: [1.38, 1.39],
+  z: [-0.85, -0.84],
 }
 //centrea spawns
 export const CENTRAL_ZONE_OCTO_BOUNDS = {
@@ -16,15 +22,15 @@ export const CENTRAL_ZONE_OCTO_BOUNDS = {
   z: [-0.4, 0.4] as [number, number],
 }
 
-export const CENTRAL_ZONE_ZEPP_BOUNDS = {
+/*export const CENTRAL_ZONE_ZEPP_BOUNDS = {
   x: [-0.65, -0.64] as [number, number],
   y: [2.737, 2.79] as [number, number],
   z: [0.1, 0.15] as [number, number],
-}
-export const RIGHT_ZONE_BOUNDS: SpawnBounds = {
-  x: [-0.58, -0.35],
-  y: [1.5, 2.2],
-  z: [-0.8, -0.75001],
+}*/
+export const CENTRAL_ZONE_ZEPP_BOUNDS = {
+  x: [-0.65, -0.64] as [number, number],
+  y: [2.5, 2.53] as [number, number],
+  z: [0.1, 0.15] as [number, number],
 }
 
 export const randomBetween = (min: number, max: number) => min + Math.random() * (max - min)
